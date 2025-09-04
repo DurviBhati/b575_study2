fd8c2cca943ae67059c8a15f25a6d63b36c04723
Durvi Bhati <durvibhati69@gmail.com>
2025-09-04 18:00:38 -0400

    Edited with the first code

--- studysession2_code.sh
+++ studysession2_code.sh
@@ -1,1 +1,5 @@
 
+
+##The code for the 1st problem, calculate the exact ammount of variants 
+cat /mnt/d/UMich/Courses_fall/bioinf575/gsm_data.txt | grep -v '#' | awk '$4 =="T" && ($5 == "C" || $5 =="G")' | wc -l
+
